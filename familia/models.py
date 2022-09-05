2834d7dcbc4cede43bfae02d7a9342d2510bd67e
Christian Rodriguez <christ.rod@gmail.com>
2022-09-05 13:40:32 -0500

    App Familia. Primera migración de familia app

--- familia/models.py
+++ familia/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class FamiliaMiembro(models.Model):
+    nombre = models.CharField(max_length=128)
+    fecha_nacimiento = models.DateField()
+    peso_kg = models.IntegerField()
+
+
+class Propiedad(models.Model):
+    nombre = models.CharField(max_length=128)
+    familia_miembro = models.ForeignKey('FamiliaMiembro', on_delete=models.CASCADE, null=True )
+
+    
